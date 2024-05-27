8584fbe0da97b66acfc1789c14ceb6b8320ad168
jeandavidgaj <jeandavidgaj@gmail.com>
2024-05-27 08:55:09 -0600

    .

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 6010
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/auth', authRoutes)
+app.use('/', authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server running in: ${PORT}`)
